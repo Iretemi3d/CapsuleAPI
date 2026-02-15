25b900523b8ae929ed6b6dc6f9621b149c9d77cb
Iretemi <161654703+Iretemi3d@users.noreply.github.com>
2026-02-15 23:53:32 +0000

    Update CapsuleAPI.java

--- src/main/java/me/Iretemi/capsuleapi/CapsuleAPI.java
+++ src/main/java/me/Iretemi/capsuleapi/CapsuleAPI.java
@@ -2,11 +2,12 @@ package me.Iretemi.capsuleapi;
 
 import me.Iretemi.capsuleapi.capsule.CapsuleAbility;
 import me.Iretemi.capsuleapi.color.CapsuleColor;
+import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 
 public interface CapsuleAPI {
 
-    ItemStack createCapsuleItem(String id, String name, String description, CapsuleColor color, CapsuleAbility ability);
+    ItemStack createCapsuleItem(String id, String name, String description, Material material, CapsuleColor color, CapsuleAbility ability);
 
 }
 
